65b2cae4d9ce8eb664ed4fda717b0f6e08e8026e
skchqhdpdy <aniren@debian.moe>
2025-12-03 23:43:40 +0900

    Update make

--- make/extract.sh
+++ make/extract.sh
@@ -1,5 +1,5 @@
-docker build -t make-riscv64-static .
-docker create --name extract make-riscv64-static
-docker cp extract:/make-riscv64 ./make-riscv64
+docker build -t make-riscv64.
+docker create --name extract make-riscv64
+docker cp extract:/make-riscv64 ./make
 docker rm extract
-docker image rm make-riscv64-static
+docker image rm make-riscv64
